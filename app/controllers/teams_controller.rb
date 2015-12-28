17edc073686ec23ed4d02911c50df444dac41630
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2015-12-28 12:14:55 -0500

    first team_controller refactor

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,11 +1,17 @@
 class TeamsController < ApplicationController
   respond_to :html, :js
 
+  before_action :fetch_team, only: [:show, :edit, :update, :request_join,
+    :leave, :add_student, :remove_student, :set_as_liaison, :accept_request]
+
+  before_action :fetch_student, only: [:remove_student, :set_as_liaison]
+
+  before_action :fetch_students, only: [:new, :add_student]
+
   def show
-    @team = Team.where(id: team_id).first
     @requests = Array.new
     @team.requests.each do |request|
-      @requests.push(Student.where(id: request.requestor_id).first)
+      @requests.push(Student.find(request.requestor_id))
     end
     if current_user.has_role?(:student)
       if current_user.teams.where(id: team_id).empty?
@@ -16,7 +22,6 @@ class TeamsController < ApplicationController
 
   def new
     @course = current_user.courses.find(course_id)
-    @students = Array.new
     @course.students.each do |student|
       @students.push(student) unless student.has_team?(@course)
     end
@@ -51,112 +56,85 @@ class TeamsController < ApplicationController
   end
 
   def edit
-    @team = current_user.teams.find(team_id)
     unless @team.valid?
       redirect_to root_path, alert: 'You cannot update this course!'
     end
   end
 
   def update
-    team = current_user.teams.find(team_id)
-    if team.update(team_params)
-      redirect_to course_path(team.course), notice: 'The course was updated successfully!'
+    if @team.update(team_params)
+      redirect_to course_path(@team.course), notice: 'The course was updated successfully!'
     else
-      redirect_to course_path(team.course), alert: 'The course failed to update.'
+      redirect_to course_path(@team.course), alert: 'The course failed to update.'
     end
   end
 
   def complete
-    team = Team.find(team_id)
-    team.set_status('complete')
-    team.save
-
-    if team.valid?
-      redirect_to team_path(team), notice: 'The team\'s status was updated successfully!'
-    else
-      redirect_to team_path(team), alert: 'The team\'s status failed to update.'
-    end
+    in_complete(team_id, 'complete')
   end
 
   def incomplete
-    team = Team.find(team_id)
-    team.set_status('incomplete')
-    team.save
-
-    if team.valid?
-      redirect_to team_path(team), notice: 'The team\'s status was updated successfully!'
-    else
-      redirect_to team_path(team), alert: 'The team\'s status failed to update.'
-    end
+    in_complete(team_id, 'incomplete')
   end
 
   def request_join
-    team = Team.find(team_id)
-    request = team.requests.create({:requestor_id => current_user.id, :status => 'pending'})
+    request = @team.requests.create({:requestor_id => current_user.id, :status => 'pending'})
     request.save
 
     if request.valid?
-      redirect_to course_path(team.course), notice: 'Your request has been sent!'
+      redirect_to course_path(@team.course), notice: 'Your request has been sent!'
     else
-      redirect_to course_path(team.course), alert: 'There was a problem sending your request.'
+      redirect_to course_path(@team.course), alert: 'There was a problem sending your request.'
     end
   end
 
   def leave
-    team = Team.find(team_id)
-    course = team.course
-    team.students.delete(current_user)
-    current_user.remove_role(:liaison, team) if current_user.has_role?(:liaison, team)
-    team.check_if_orphaned_after_user_removed
-    redirect_to course_path(course), notice: 'You have been removed from the team.'
+    @team.students.delete(current_user)
+    current_user.remove_role(:liaison, @team) if current_user.has_role?(:liaison, @team)
+    @team.check_if_orphaned_after_user_removed
+    redirect_to course_path(@team.course), notice: 'You have been removed from the team.'
   end
 
   def add_student
     if request.get?
-      @team = Team.find(team_id)
-      @students = Array.new
       @team.course.students.each do |student|
         @students.push(student) unless student.has_team?(@team.course)
       end
     elsif request.post?
-      team = Team.find(team_id)
-      params[:team][:student_ids] = params[:team][:student_ids][0, team.max_students - team.students.count]
-      params[:team][:student_ids].each do |student_id|
-        team.students << Student.find(student_id) unless student_id.blank?
+      if @team.max_students < @team.students.count + student_ids.count
+        redirect_to team_path(@team), alert: 'Too many students selected!'
+      else
+        student_ids.each do |student_id|
+          @team.students << Student.find(student_id) unless student_id.blank?
+        end
+        redirect_to team_path(@team), notice: 'Students have been added successfully.'
       end
-      redirect_to team_path(team), notice: 'Students have been added successfully!'
     end
   end
 
   def remove_student
-    team = Team.find(team_id)
-    course = team.course
-    student = Student.find(student_id)
-    team.students.delete(student)
-    if (team.check_if_orphaned_after_user_removed)
-      redirect_to course_path(course), notice: 'Student has been removed fromt he team.'
+    @team.students.delete(@student)
+    if (@team.check_if_orphaned_after_user_removed)
+      redirect_to course_path(@team.course), notice: 'Student has been removed from the team.'
     else
-      redirect_to team_path(team), notice: 'Student has been removed fromt he team.'
+      redirect_to team_path(@team), notice: 'Student has been removed from the team.'
     end
   end
 
   def set_as_liaison
-    team = Team.find(team_id)
-    student = Student.find(student_id)
-    current_user.remove_role(:liaison, team)
-    student.add_role(:liaison, team)
-    redirect_to team_path(team), notice: 'New team liaison has been set.'
+    current_user.remove_role(:liaison, @team)
+    @student.add_role(:liaison, @team)
+    redirect_to team_path(@team), notice: 'New team liaison has been set.'
   end
 
   def accept_request
-    team = Team.find(team_id)
-    if team.is_complete?
-      redirect_to team_path(team), alert: 'Cannot add new member to full team!'
+    if @team.is_complete?
+      redirect_to team_path(@team), alert: 'Cannot add new member to full team!'
     else
-      team.students << Student.find(student_id)
-      request = team.requests.where(requestor_id: student_id).first
+      @team.students << Student.find(student_id)
+      request = @team.requests.where(requestor_id: student_id).first
       request.destroy
-      redirect_to team_path(team), notice: 'New member added to team.'
+      redirect_to team_path(@team), notice: 'New member added to team.'
     end
   end
 
@@ -176,5 +154,33 @@ class TeamsController < ApplicationController
 
     def student_id
       params.require(:student_id)
+    end
+
+    def student_ids
+      params.require(:team).permit(:student_ids => [])[:student_ids].reject!(&:empty?)
+    end
+
+    def in_complete(team_id, status)
+      team = Team.find(team_id)
+      team.set_status(status)
+      team.save
+
+      if team.valid?
+        redirect_to team_path(team), notice: 'The team\'s status was updated successfully!'
+      else
+        redirect_to team_path(team), alert: 'The team\'s status failed to update.'
+      end
+    end
+
+    def fetch_team
+      @team = Team.find(team_id)
+    end
+
+    def fetch_student
+      @student = Student.find(student_id)
+    end
+
+    def fetch_students
+      @students = Array.new
     end
 end
